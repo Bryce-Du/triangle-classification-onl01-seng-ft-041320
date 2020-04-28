f207d32b2e2031385512164e51e77955b5f47c9f
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 20:06:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,7 @@ class Triangle
   
   def kind
     if (@a + @b < @c || @a + @c < @b || @b + @c < @a || @a <= 0 || @b <= 0 || @c <= 0 )
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
     elsif (@a == @b && @b == @c && @c == @a)
       :equilateral
     elsif (@a == @b || @b == @c || @a == @c)
