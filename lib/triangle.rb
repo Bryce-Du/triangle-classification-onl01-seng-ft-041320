58e5800bf60adf291f2425f45ff83a2497535e72
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 20:05:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,11 @@ class Triangle
   
   def kind
     if (@a + @b < @c || @a + @c < @b || @b + @c < @a || @a <= 0 || @b <= 0 || @c <= 0 )
-      raise TriangleError
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
     else if (@a == @b && @b == @c && @c == @a)
       :equilateral
     else if (@a == @b || @b == @c || @a == @c)
