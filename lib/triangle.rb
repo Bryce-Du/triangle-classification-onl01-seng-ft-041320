9e9a9e28218c427ad94891c331c9d340d3b600d1
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 20:04:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c
+  end
+  
+  def kind
+    if (@a + @b < @c || @a + @c < @b || @b + @c < @a || @a <= 0 || @b <= 0 || @c <= 0 )
+      raise TriangleError
+    else if (@a == @b && @b == @c && @c == @a)
+      :equilateral
+    else if (@a == @b || @b == @c || @a == @c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
